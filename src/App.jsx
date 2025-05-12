add06dc8056d61bdc9be2e6261bd8ddab284ae5e
Manikumar <allemanikumar363@gmail.com>
2025-05-12 18:08:22 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+import ChatScreen from "./pages/ChatScreen";
+
+function App() {
+ 
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ChatScreen />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
